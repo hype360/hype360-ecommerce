60cdcadcca15456787bdd0d9beb2fd0c2722150f
Chamrong THOR <chamrong.thor@allweb.com.kh>
2025-02-11 13:49:48 +0700

    Add Notification

---
+++ services/service-notification/src/test/java/com/hype360kh/servicenotification/ServiceNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hype360kh.servicenotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceNotificationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
